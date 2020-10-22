edb22823334d41df800e2ea3820140acee180568
Claire DeBoer <claire.deboer@gmail.com>
2020-10-22 21:04:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-  if string[0] == 's'
-    puts 'ssssssssss' + string
+  if string[0] == "s"
+    puts "ssssssssss" + string
   else
     puts string
   end
