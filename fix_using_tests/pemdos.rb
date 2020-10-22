e3dd1c3198bdeab887bcb825f918383ee356ef8b
Claire DeBoer <claire.deboer@gmail.com>
2020-10-22 21:18:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,10 +2,8 @@
 require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    binding.pry
-    new_string = "ssssssssss" + string
+    "ssssssssss" + string
   else
     string
-    binding.pry
   end
 end
